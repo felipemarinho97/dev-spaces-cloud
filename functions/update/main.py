50e3a27b385efdf9b9c824450604b5118156848a
Felipe Marinho <felipevm97@gmail.com>
2023-08-16 21:48:05 +0000

    new: feat: add update function as separate code

---
+++ functions/update/main.py
@@ -0,0 +1,53 @@
+import boto3
+import json
+
+client = boto3.client('route53')
+
+def lambda_handler(event, context):
+    params = event['queryStringParameters']
+    # get IP from query string
+    ip = params.get('ip')
+    if not ip:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({"error": "IP is required"})
+        }
+
+    # get name from query string
+    name = params.get('domain')
+    if not name:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({"error": "Name is required"})
+        }
+
+    # Get the hosted zone ID from env variable
+    hosted_zone_id = os.environ['HOSTED_ZONE_ID']
+
+    # add a new record
+    response = client.change_resource_record_sets(
+        HostedZoneId=hosted_zone_id,
+        ChangeBatch={
+            'Comment': 'Add a new record',
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': name + '.devspaces.online',
+                        'Type': 'A',
+                        'TTL': 300,
+                        'ResourceRecords': [
+                            {
+                                'Value': ip
+                            },
+                        ],
+                    }
+                },
+            ]
+        }
+    )
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"address": name + '.devspaces.online'})
+    }
